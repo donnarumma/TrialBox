28f0296cbe9896e0c99048c939c0edfb6474a41c
Mircuthio <mircofrosolone@gmail.com>
2026-04-14 11:58:33 +0200

    modify selectionLFP and alignBattaglia

--- utilities/alignBattaglia.m
+++ utilities/alignBattaglia.m
@@ -1,17 +1,33 @@
 function data_trials = alignBattaglia(data_trials,par)
 
 AlignEvent = par.AlignEvent;
-InField = par.InField;
-OutField = par.OutField;
-xfld        = 'time';
-xfld_old    = 'oldTime'; 
+LockEvent  = par.LockEvent;
+InField    = par.InField;
+OutField   = par.OutField;
 
-TimeLock = nan(1,length(data_trials));
-for iTr = 1:length(data_trials)
-    TimeLock(iTr) = data_trials(iTr).Event (data_trials(iTr).Event (:,1)==AlignEvent,2);
-end
+xfld     = 'time';
+xfld_old = 'oldTime';
 
 for iTr = 1:length(data_trials)
+
+    events = data_trials(iTr).Event;
+
+    idxAlign = find(events(:,1) == AlignEvent, 1);
+    idxLock  = find(events(:,1) == LockEvent, 1);
+
+    if isempty(idxAlign)
+        warning('AlignEvent non trovato nel trial %d', iTr);
+        continue
+    end
+
+    tAlign = events(idxAlign,2);
+
     data_trials(iTr).([xfld_old InField]) = data_trials(iTr).([xfld InField]);
-    data_trials(iTr).([xfld OutField])  = data_trials(iTr).([xfld InField])  - TimeLock(iTr);
+    data_trials(iTr).([xfld OutField])    = data_trials(iTr).([xfld InField]) - tAlign;
+
+    if ~isempty(idxLock) && isfield(data_trials(iTr),'ET') && ~isempty(data_trials(iTr).ET)
+        tLock = events(idxLock,2);
+        delta = tLock - tAlign;
+        data_trials(iTr).ET = data_trials(iTr).ET + delta;
+    end
 end
